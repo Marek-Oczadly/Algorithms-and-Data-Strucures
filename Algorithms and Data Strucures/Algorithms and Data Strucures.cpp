0346576ed44e0acf19d76cacc603bd3d7c169c01
Marek Oczadly <moczadly@outlook.com>
2025-02-18 19:27:40 +0000

    Fixed an issue with my selection sort

--- Algorithms and Data Strucures/Algorithms and Data Strucures.cpp
+++ Algorithms and Data Strucures/Algorithms and Data Strucures.cpp
@@ -1,20 +1,13 @@
-// Algorithms and Data Strucures.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "SelectionSort.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+int main() {
+	int x[] = { 3, 5, 2, 7, 2, 6, 2, 6, 8, 11 };
+	const unsigned short SIZE = sizeof(x) / sizeof(int);
+	SelectionSort(&x[0], &x[0] + SIZE);
+	for (const int* i = &x[0]; i < &x[0] + SIZE; ++i) {
+		std::cout << *i << "	  " << std::endl;
+	}
+	std::cout << std::endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- Algorithms and Data Strucures/SelectionSort.h
+++ Algorithms and Data Strucures/SelectionSort.h
@@ -3,9 +3,9 @@
 
 template <typename T>
 void SelectionSort(T* start, T* end) {
-	for (T* j = start; j < end - 1; ++j) {
-		T* smallest = start;
-		for (T* i = j + 1; i < end; ++i) {
+	for (T* j = start; j < end; ++j) {
+		T* smallest = j;
+		for (T* i = j; i < end; ++i) {
 			if (*i < *smallest) { smallest = i; }
 		}
 		const T temp = *smallest;

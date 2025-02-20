32ecc8eecde0dc6d097cc071381378fff65102ce
Marek Oczadly <moczadly@outlook.com>
2025-02-20 15:03:28 +0000

    Documented and commented on all my functions

--- Algorithms and Data Strucures/sorting_algorithms.h
+++ Algorithms and Data Strucures/sorting_algorithms.h
@@ -1,11 +1,17 @@
 #pragma once
-#include <algorithm>
 
+/// @brief A series of static functions for sorting data
+/// @tparam T the type of the data being sorted
 template <typename T>
 class SortingAlgorithms {
 private:
-	typedef void(*FuncPtr)(T*, T*);
+	/// @brief Typedef for a sorting function
+	typedef void(*SortFunction)(T*, T*);
 
+	/// @brief Merges 2 sorted arrays in a merge sort
+	/// @param start The start of the array being merged
+	/// @param mid The midpoint between the 2 arrays being merged
+	/// @param end The end point of the array being merged
 	static void merge(T* start, T* mid, T* end) {
 		const unsigned int n_l = mid - start + 1;
 		const unsigned int n_r = end - mid;
@@ -25,6 +31,9 @@ private:
 		delete[] L;
 	}
 
+	/// @brief Recursive function to implement a merge sort on an array
+	/// @param start Start of the array being sorted
+	/// @param end Inclusive end of the array being sorted
 	static void mergeSort_(T* start, T* end) {
 		if (start < end) {	// True if the start and end are greater than 1
 			T* const midpoint = start + ((end - start) / 2);
@@ -35,6 +44,9 @@ private:
 	}
 
 public:
+	/// @brief Implements the selection sort algorithm to sort an array
+	/// @param start The start of the array being sorted
+	/// @param end The end of the array being sorted
 	static void selectionSort(T* start, T* end) {
 		for (T* j = start; j < end; ++j) {
 			T* smallest = j;
@@ -47,6 +59,9 @@ public:
 		}
 	}
 
+	/// @brief Implements the insertion sort algorithm to sort an array
+	/// @param start The start of the array being sorted
+	/// @param end The end of the array being sorted
 	static void insertionSort(T* start, T* end) {
 		for (T* j = start + 1; j < end; ++j) {
 			const T key = *j;
@@ -59,19 +74,22 @@ public:
 		}
 	}
 
+	/// @brief Uses the merge sort algorithm to sort an array
+	/// @param start Start of the array being sorted
+	/// @param end End (not inclusive) of the array being sorted
 	static void mergeSort(T* start, T* end) {
 		mergeSort_(start, end-1);
 	}
 
-	static bool isCorrect(T* start, T* end, FuncPtr func) {
-		const unsigned int size = end - start;
-		T* copy = new T[size];
-		std::copy(start, end, copy);
-		std::sort(copy, copy + size);
+	/// @brief Checks if a sorting algorithm sorts the array in ascending order
+	/// @param start - The start of the array being sorted
+	/// @param end - The end of the array being sorted
+	/// @param func - The sorting function being tested
+	/// @return - True if the array is sorted after being passed through the algorithm
+	static bool isCorrect(T* start, T* end, SortFunction func) {
 		func(start, end);
-
-		for (int i = 0; i < size; ++i) {
-			if (copy[i] != *(start + i)) { return false; }
+		for (const T* i = start + 1; i < end; ++i) {
+			if (*(i - 1) > *i) return false;
 		}
 		return true;
 	}

d851ae845fc358e121b4060a360270541101d6da
Marek Oczadly <moczadly@outlook.com>
2025-02-25 21:43:27 +0000

    Fixed my ComparisonCounter class to work with merge sort

--- Algorithms and Data Strucures/comparison_counter.h
+++ Algorithms and Data Strucures/comparison_counter.h
@@ -1,43 +1,73 @@
 #pragma once
+#include <iostream>
 
+/// @brief Wrapper class for any arithmetic class that counts how many comparisons are done to evaluate the effiency of a sorting algorithm
+/// @tparam T Any arithmetic type that is to be sorted. Tested with int, double, float.
 template <typename T>
 class ComparisonCounter {
 	T value;
 	static void increment() {
-		ComparisonCounter::count++;
+		ComparisonCounter<T>::count++;
 	}
 public:
 	static unsigned int count;
 	static unsigned int getCount() noexcept {
-		return count;
+		return ComparisonCounter<T>::count;
 	}
-
+	static void resetCount() noexcept {
+		ComparisonCounter<T>::count = 0;
+	}
+	ComparisonCounter() {}
 	ComparisonCounter(const T& value) : value(value) {}
 	ComparisonCounter(const ComparisonCounter<T>& other) {
 		value = other.value;
 	}
 
-	bool operator >(const ComparisonCounter<T>& other) {
+	bool operator >(const ComparisonCounter<T>& other) const noexcept {
 		ComparisonCounter<T>::increment();
 		return value > other.value;
 	}
-	bool operator <(const ComparisonCounter<T>& other) {
+	bool operator <(const ComparisonCounter<T>& other) const noexcept {
 		ComparisonCounter<T>::increment();
-		return value > other.value;
+		return value < other.value;
 	}
-	bool operator ==(const ComparisonCounter<T>& other) {
+	bool operator ==(const ComparisonCounter<T>& other) const noexcept {
 		ComparisonCounter<T>::increment();
 		return value == other.value;
 	}
-	bool operator >=(const ComparisonCounter<T>& other) {
+	bool operator >=(const ComparisonCounter<T>& other) const noexcept {
 		ComparisonCounter<T>::increment();
 		return value >= other.value;
 	}
-	bool operator <=(const ComparisonCounter<T>& other) {
+	bool operator <=(const ComparisonCounter<T>& other) const noexcept {
 		ComparisonCounter<T>::increment();
 		return value <= other.value;
 	}
-	operator T& () {
+	bool operator >(const T& other) const noexcept {
+		ComparisonCounter<T>::increment();
+		return value > other;
+	}
+	bool operator <(const T& other) const noexcept {
+		ComparisonCounter<T>::increment();
+		return value < other;
+	}
+	bool operator ==(const T& other) const noexcept {
+		ComparisonCounter<T>::increment();
+		return value == other;
+	}
+	bool operator >=(const T& other) const noexcept{
+		ComparisonCounter<T>::increment();
+		return value >= other;
+	}
+	bool operator <=(const T& other) const noexcept {
+		ComparisonCounter<T>::increment();
+		return value <= other;
+	}
+	operator T& () noexcept {
+		return value;
+	}
+
+	operator const T& () const noexcept {
 		return value;
 	}
 	ComparisonCounter<T>& operator =(const ComparisonCounter<T>& other) {
